3b7669c64978d87365791b84d9bf5feaab014868
Arnav Sachdeva <arnavsachdeva@proton.me>
2026-03-24 13:59:46 +0530

    Upload

---
+++ Class 7/prog1.cpp
@@ -0,0 +1,31 @@
+/*Create a class Person with data members name and age, and a function displayPerson(). Derive a class Student that adds roll number and a function displayStudent(). Show how a Student object can both base and derived class functions.*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+protected:
+    string name;
+    int age;
+
+public:
+    Person(string n, int a) : name(n), age(a) {}
+
+    void displayPerson() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+class Student : public Person {
+private:
+    int rollNumber;
+
+public:
+    Student(string n, int a, int roll) : Person(n, a), rollNumber(roll) {}
+
+    void displayStudent()
+    {
+        displayPerson();
+        cout << "Roll Number: " << rollNumber << endl;
+    }
+};
